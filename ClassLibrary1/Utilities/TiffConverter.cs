16518d1f8c0cbbf586bf40be649972aa9e43ba2e
Gevand <gevand@outlook.com>
2018-02-03 17:48:56 -0800

    Splitter figured out

---
+++ ClassLibrary1/Utilities/TiffConverter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+using BitMiracle.LibTiff.Classic;
+using System.Drawing.Imaging;
+
+namespace Neural.Utilities
+{
+    public class TiffConverter
+    {
+        public static int Split(string tiffFilePath, int splitPixelSize = 20)
+        {
+            using (Tiff image = Tiff.Open(tiffFilePath, "r"))
+            {
+                // Find the width and height of the image
+                FieldValue[] value = image.GetField(TiffTag.IMAGEWIDTH);
+                int width = value[0].ToInt();
+
+                value = image.GetField(TiffTag.IMAGELENGTH);
+                int height = value[0].ToInt();
+
+                int imageSize = height * width;
+                int[] raster = new int[imageSize];
+                image.ReadRGBAImage(width, height, raster);
+                int total = imageSize / splitPixelSize;
+                int count = 0;
+                // Read the image into the memory width 
+                while (total >= count)
+                {
+                    try
+                    {
+                        using (Bitmap bmp = new Bitmap(splitPixelSize, splitPixelSize))
+                        {
+                            for (int i = 0; i < bmp.Width; ++i)
+                                for (int j = 0; j < bmp.Height; ++j)
+                                {
+                                    int x = i + (count * splitPixelSize);
+                                    int y = j + (count * splitPixelSize);
+
+                                    int offset = (height - y - 1) * width + x;
+                                    int red = Tiff.GetA(raster[offset]);
+                                    int green = Tiff.GetR(raster[offset]);
+                                    int blue = Tiff.GetG(raster[offset]);
+                                    bmp.SetPixel(i, j, Color.FromArgb(red, green, blue));
+                                }
+                            bmp.Save(String.Format("test{0}.bmp", count));
+                        }
+                    }
+                    catch { }
+                    finally { count++; }
+                }
+                return count;
+            }
+        }
+    }
+}
---
+++ Neural.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Neural.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Neural.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("139f77b1-e0c8-4cf0-839d-33a6675cb60b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
